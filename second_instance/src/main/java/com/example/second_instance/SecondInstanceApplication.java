0bbee9d883ae99bd7f2eefabab6cf86a486bb8f7
mostafijurr <mostafijur.rahman@brainstation23.com>
2021-11-03 16:44:26 +0600

    extra files added

---
+++ second_instance/src/main/java/com/example/second_instance/SecondInstanceApplication.java
@@ -0,0 +1,13 @@
+package com.example.second_instance;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecondInstanceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SecondInstanceApplication.class, args);
+    }
+
+}
---
+++ second_instance/src/test/java/com/example/second_instance/SecondInstanceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.second_instance;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecondInstanceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
